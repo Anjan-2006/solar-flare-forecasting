5d6d3ed75fe5036627ea71e7d1e0685f638b8fb4
Anjan-2006 <pendemanjan2006@gmail.com>
2026-05-20 00:44:19 +0530

    changed the localhost url to the ec2 server ip

--- src/pages/LiveForecast.jsx
+++ src/pages/LiveForecast.jsx
@@ -6,7 +6,7 @@ import { jsPDF } from 'jspdf';
 import { AreaChart, Area, ResponsiveContainer, YAxis, XAxis, Tooltip, CartesianGrid } from 'recharts';
 import { useLiveStore } from '../store/useLiveStore';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://65.0.89.218:8000';
 
 const getBase64ImageFromUrl = async (imageUrl) => {
   try {
