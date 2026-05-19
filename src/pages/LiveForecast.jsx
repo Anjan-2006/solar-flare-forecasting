375ebc8bdf5448f5948ee5a8f620bfc4c06256d8
Anjan-2006 <pendemanjan2006@gmail.com>
2026-05-20 02:16:54 +0530

    changing the api end point due to the browser issues

--- src/pages/LiveForecast.jsx
+++ src/pages/LiveForecast.jsx
@@ -6,7 +6,7 @@ import { jsPDF } from 'jspdf';
 import { AreaChart, Area, ResponsiveContainer, YAxis, XAxis, Tooltip, CartesianGrid } from 'recharts';
 import { useLiveStore } from '../store/useLiveStore';
 
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://solarflare.anjan.top';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://api.anjan.top';
 
 const getBase64ImageFromUrl = async (imageUrl) => {
   try {
